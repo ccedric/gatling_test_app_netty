ff0ac7baf172d63a7e0f0123be8acb9aead52fa9
Cédric Cousseran <cedric.cousseran@gmail.com>
2016-06-21 15:52:51 +0200

    add conf

--- src/test/scala/io/gatling/netty/benchmark/FastWorkload.scala
+++ src/test/scala/io/gatling/netty/benchmark/FastWorkload.scala
@@ -1,14 +1,18 @@
 package io.gatling.netty.benchmark
 
+import com.typesafe.config.ConfigFactory
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
 
 
 class FastWorkload extends Simulation {
 
+  val config = ConfigFactory.load()
+
   val httpConf = http
-    .baseURL("http://localhost:8000") // Here is the root for all relative URLs
+    .baseURL(config.getString("frontline.benchmark.netty.webUrl")) // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
@@ -36,6 +40,6 @@ class FastWorkload extends Simulation {
 
   setUp(
     scn.inject(
-      atOnceUsers(2000)))
+      atOnceUsers(config.getInt("frontline.benchmark.netty.users"))))
     .protocols(httpConf)
 }
