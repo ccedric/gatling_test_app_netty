771787984d7a8b4ccad76abd59595c6777b6d59a
Cédric Cousseran <cedric.cousseran@gmail.com>
2016-06-22 12:15:24 +0200

    Keep only hello request

--- src/test/scala/io/gatling/netty/benchmark/FastWorkload.scala
+++ src/test/scala/io/gatling/netty/benchmark/FastWorkload.scala
@@ -20,25 +20,10 @@ class FastWorkload extends Simulation {
 
   val scn = scenario("scenario1")
     .during(config.getInt("frontline.benchmark.netty.duration") seconds) {
-      group("hello"){
         exec(
           http("hello")
             .get("/hello")
             .check(status.is(200)))
-      }.group("json"){
-        exec(
-          http("json1k")
-            .get("/json1k")
-            .check(status.is(200)))
-          .exec(
-            http("json10k")
-              .get("/json10k")
-              .check(status.is(200)))
-          .exec(
-            http("json100k")
-              .get("/json100k")
-              .check(status.is(200)))
-      }
     }
 
   setUp(
